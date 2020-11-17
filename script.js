f2065aacbe1b34f53f87f554e73eba3589249859
Johnny Kleyson <kleysonjohnny2016@gmail.com>
2020-11-17 18:41:38 -0300

    form validation

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+//Scroll to Top Button fixed on screen
 const btnScrollTop = document.querySelector(".scroll-to-top");
 btnScrollTop.addEventListener("click", function(){
     window.scrollTo(0,0);
 })
+
+
+//Set page to not Refresh
+
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+//Form Validation
+
+
+function validate(){
+    var name = document.getElementById("name").value; 
+    var email = document.getElementById("email").value;
+    var message = document.querySelector(".form-textarea").value;
+    var error_message = document.getElementById("error_message");
+
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length < 3){
+      text = "Por favor insira um nome.";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Por favor ensira um email válido";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length == 0){
+      text = "Por favor escreva sua mensagem";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <5 ){
+      text = "Por favor escreva um pouco mais";
+      error_message.innerHTML = text;
+      return false;
+    }
+
+    alert("Mensagem Enviada com sucesso!");
+    return true;
+  }
